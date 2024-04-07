0ba89ab4b18b7ac8c54826202beeff7d7f6d4519
Marcos Figueiredo <132932118+mapadufi@users.noreply.github.com>
2024-04-06 22:39:19 -0300

    Atualização lógica para salvar
    Atualização lógica para salvar

--- app/src/main/java/com/example/notas/CadNotaActivity.kt
+++ app/src/main/java/com/example/notas/CadNotaActivity.kt
@@ -2,14 +2,59 @@ package com.example.notas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.notas.dao.NotaDao
 import com.example.notas.databinding.ActivityCadNotaBinding
+import com.example.notas.model.Nota
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class CadNotaActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCadNotaBinding
+    private lateinit var notaDao: NotaDao
+    private val listaNotas: MutableList<Nota> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCadNotaBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+            binding.btCadSal.setOnClickListener{
+
+                CoroutineScope(Dispatchers.IO).launch {
+
+                val titulo = binding.edtCadTitulo.text.toString()
+                val anotacao = binding.edtCadAnotacao.text.toString()
+                val mensagem: Boolean
+
+                if (titulo.isEmpty() || anotacao.isEmpty()){
+                    mensagem = false
+                }else {
+                    mensagem = true
+                    cadastrarNota(titulo, anotacao)
+                }
+
+                withContext(Dispatchers.Main){
+                    if (mensagem){
+                        Toast.makeText(applicationContext, "Anotação Salva", Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(applicationContext, "Preencha todos os campos", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun cadastrarNota(titulo: String, anotacao: String){
+        val nota = Nota(titulo, anotacao)
+        listaNotas.add(nota)
+        notaDao = AppDatabase.getInstance(this).notasDao()
+        notaDao.inserir(listaNotas)
+
+
     }
 }
